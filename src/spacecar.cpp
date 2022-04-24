a329076cd12bd8239b9909db3ae769f07b5d8849
mehmet duramaz <56667665+mduramaz@users.noreply.github.com>
2022-04-24 18:55:37 +0300

    Update spacecar.cpp

--- src/spacecar.cpp
+++ src/spacecar.cpp
@@ -2,11 +2,9 @@
 #include "params.h"
 Spacecar::Spacecar(int centerx,int centery,int sizex,int sizey,Renderer &renderer,int direction):centerx(centerx),centery(centery),sizex(sizex),sizey(sizey),renderer(&renderer)
 {
-
-this->direction=direction;
-weapon=new Weapon(centerx,centery,sizeBulletx,sizeBullety,speedBullet,sizex,sizey,&renderer,direction);
-renderer.CreateVisualCar(centerx,centery,sizex,sizey);
-
+  this->direction=direction;
+  weapon=new Weapon(centerx,centery,sizeBulletx,sizeBullety,speedBullet,sizex,sizey,&renderer,direction);
+  renderer.CreateVisualCar(centerx,centery,sizex,sizey);
 }  
 
 void Spacecar::move(int x,int y)
