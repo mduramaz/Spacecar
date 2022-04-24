504e38259d10bd563662a97ec1d9c2463e402a4b
mehmet duramaz <56667665+mduramaz@users.noreply.github.com>
2022-04-24 15:37:43 +0300

    Update spacecar.h

--- include/spacecar.h
+++ include/spacecar.h
@@ -3,20 +3,20 @@
 
 class Spacecar{
 	public:
-              Spacecar(int centerx,int centery,int sizex,int sizey,Renderer &renderer,int direction=0);
-              void updateVisual();
-              void move(int x,int y);
-              Weapon *weapon;
-              int getsizex()const{return sizex;}  
-              int getsizey()const{return sizey;}  
-              int getcenterx()const{return centerx;}  
-              int getcentery()const{return centery;}  
+           Spacecar(int centerx,int centery,int sizex,int sizey,Renderer &renderer,int direction=0);
+           void updateVisual();
+           void move(int x,int y);
+           Weapon *weapon;
+           int getsizex()const{return sizex;}  
+           int getsizey()const{return sizey;}  
+           int getcenterx()const{return centerx;}  
+           int getcentery()const{return centery;}  
 
 	private:
-            int sizex;
-            int sizey;
-            int centerx;
-            int centery;
-            int direction=0;
-            Renderer *renderer; 
+           int sizex;
+           int sizey;
+           int centerx;
+           int centery;
+           int direction=0;
+           Renderer *renderer; 
 };
