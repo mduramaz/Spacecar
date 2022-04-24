addf8d53738e9ca116ecc4e322825dd52ee8e0ab
mehmet duramaz <56667665+mduramaz@users.noreply.github.com>
2022-04-24 15:39:58 +0300

    Update bullet.h

--- include/bullet.h
+++ include/bullet.h
@@ -9,7 +9,7 @@ class Bullet
         Renderer *renderer;
         int direction{0};
    public:
-	    Bullet(int sizex,int sizey,int x,int y,Renderer *renderer,int direction=0);
+	Bullet(int sizex,int sizey,int x,int y,Renderer *renderer,int direction=0);
 
         int getsizex()const{return sizex;}
         int getsizey()const{return sizey;}
